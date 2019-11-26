6290a23c9065a9c24112171af31354a483d31143
Nitin Chaurasia <gs.nitin@gmail.com>
2019-11-25 23:39:21 -0500

    first commit

---
+++ src/test/java/com/spring5/introduction/spring5introduction/Spring5IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.introduction.spring5introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5IntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
