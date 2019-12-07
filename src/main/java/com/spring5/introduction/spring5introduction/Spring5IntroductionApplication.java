f930af80c2a8336be38659a07a705c3017fbb98e
Nitin Chaurasia <gs.nitin@gmail.com>
2019-12-07 14:05:46 -0500

    All Beans Printed that participate with Spring Boot

--- src/main/java/com/spring5/introduction/spring5introduction/Spring5IntroductionApplication.java
+++ src/main/java/com/spring5/introduction/spring5introduction/Spring5IntroductionApplication.java
@@ -2,12 +2,16 @@ package com.spring5.introduction.spring5introduction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring5IntroductionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5IntroductionApplication.class, args);
-	}
+		ApplicationContext applicationContext = SpringApplication.run(Spring5IntroductionApplication.class, args);
 
+		for(String name : applicationContext.getBeanDefinitionNames()){
+			System.out.println(name);
+		}
+	}
 }
